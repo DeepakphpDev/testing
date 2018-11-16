cc2b4a6f29f39cd196a3c7e0310d0ce221abc56a
deepakphpDev <deepak1_him@rediff.com>
2018-11-16 11:46:16 +0530

    inde file update

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<script src="jquery-3.2.1.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	// in this example, 'foo' is a div containing button 'bar'
+	$("#foo").click(function() {
+	   alert('mouse click on div');
+	});
+	$("#bar").click(function(e) {
+	   // mouse click on button 'bar'
+	   e.stopPropagation();
+	   alert('mouse click on button');
+	   
+	});
+});  
+</script>
+</head>
+<body>
+<div id="foo" width="500" style="background-color:green"><button id="bar">Clik</button></div>
+</body>
+</html>
